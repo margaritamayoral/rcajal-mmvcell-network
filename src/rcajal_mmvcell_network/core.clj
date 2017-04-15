9da29df76ffe293e851ee77be139f92299bbd630
Margarita Mayoral Villa <margaritamayoralvilla@Margaritas-MacBook-Air.local>
2017-04-15 19:57:46 -0400

    first commit

---
+++ src/rcajal_mmvcell_network/core.clj
@@ -0,0 +1,7 @@
+(ns rcajal-mmvcell-network.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/rcajal_mmvcell_network/core_test.clj
@@ -0,0 +1,7 @@
+(ns rcajal-mmvcell-network.core-test
+  (:require [clojure.test :refer :all]
+            [rcajal-mmvcell-network.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
